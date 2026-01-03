25dcd7688efdb043c37e92180dc6a4cd2f097828
rijuvan <rijuvan.ansari@yahoo.com>
2026-01-03 12:19:02 +0530

    Add Maven POM file for project setup

---
+++ javademo/CalculateTax.java
@@ -0,0 +1,9 @@
+public class CalculateTax implements Tax {
+    
+    @Override
+    public double calculateTax(double amount) {
+        // TODO: Implement tax calculation logic
+        return 0.0;
+    }
+}
+
