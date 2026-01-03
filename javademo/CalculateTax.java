90de7c8b34a1a2f6702cda51e1210b63910f7852
rijuvan <rijuvan.ansari@yahoo.com>
2026-01-03 12:20:23 +0530

    Update Maven POM file with new dependencies and configurations

--- javademo/CalculateTax.java
+++
@@ -1,9 +0,0 @@
-public class CalculateTax implements Tax {
-    
-    @Override
-    public double calculateTax(double amount) {
-        // TODO: Implement tax calculation logic
-        return 0.0;
-    }
-}
-
